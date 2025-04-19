c11ec93299549463b7f9cbd4f1a65c7223861303
Dongho Kim <dongre2527@gmail.com>
2025-04-19 14:00:51 +0900

    useAudioEvent 훅 추가

---
+++ src/hooks/useAudioEvent.ts
@@ -0,0 +1,22 @@
+import { useEffect, type RefObject } from "react";
+import { invariant } from "../lib/invariant";
+import { useStableCallback } from "./useStableCallback";
+
+export function useAudioEvent<T extends keyof HTMLMediaElementEventMap>(
+  audioRef: RefObject<HTMLAudioElement>,
+  type: T,
+  listener: (event: HTMLMediaElementEventMap[T]) => any,
+  options?: boolean | AddEventListenerOptions
+) {
+  const stableListener = useStableCallback(listener);
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+    invariant(audioElement !== null);
+
+    audioElement.addEventListener(type, stableListener, options);
+    return () => {
+      audioElement.removeEventListener(type, stableListener, options);
+    };
+  }, [stableListener]);
+}
