b7222a8dc5d85acf03266b51b05446b8c87cad49
Dongho Kim <dongre2527@gmail.com>
2025-04-20 15:56:50 +0900

    useIsCommandKeyPressed 훅 리팩토링

--- src/hooks/useIsCommandKeyPressed.ts
+++ src/hooks/useIsCommandKeyPressed.ts
@@ -15,18 +15,16 @@ const isMacOS = navigator.userAgentData.platform === "macOS";
 export function useIsCommandKeyPressed() {
   const [isCommandKeyPressed, setIsCommandKeyPressed] = useState(false);
 
-  function handleKeyDown(event: KeyboardEvent) {
+  useWindowEventListener("keydown", (event) => {
     const isCommandKeyPressed = isMacOS ? event.metaKey : event.ctrlKey;
     if (isCommandKeyPressed) {
       setIsCommandKeyPressed(true);
     }
-  }
-  useWindowEventListener("keydown", handleKeyDown);
+  });
 
-  function handleKeyUp() {
+  useWindowEventListener("keyup", () => {
     setIsCommandKeyPressed(false);
-  }
-  useWindowEventListener("keyup", handleKeyUp);
+  });
 
   return isCommandKeyPressed;
 }
