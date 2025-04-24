03884f526d18ba08ef395878d1644ee8806bbaef
Dongho Kim <dongre2527@gmail.com>
2025-04-24 22:44:57 +0900

    한글은 NotoSansKR, 나머지는 Inter 폰트 사용하도록 변경

--- src/Content.tsx
+++ src/Content.tsx
@@ -2,18 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
-const container = document.createElement("div");
-document.body.appendChild(container);
-
 const fontUrl = chrome.runtime.getURL("static/fonts/");
 
-const shadowRoot = container.attachShadow({ mode: "open" });
-const styleElement = document.createElement("style");
-styleElement.textContent = `
+const container = document.createElement("div");
+document.body.appendChild(container);
+const style2 = document.createElement("style");
+style2.textContent = `
 @font-face {
   font-family: 'Inter';
   font-style: normal;
   font-weight: 400;
+  font-display: swap;
   src: url('${fontUrl}Inter-Regular.woff2') format('woff2');
 }
 
@@ -21,6 +20,7 @@ styleElement.textContent = `
   font-family: 'Inter';
   font-style: normal;
   font-weight: 500;
+  font-display: swap;
   src: url('${fontUrl}Inter-Medium.woff2') format('woff2');
 }
 
@@ -28,6 +28,7 @@ styleElement.textContent = `
   font-family: 'Inter';
   font-style: italic;
   font-weight: 500;
+  font-display: swap;
   src: url('${fontUrl}Inter-MediumItalic.woff2') format('woff2');
 }
 
@@ -35,15 +36,48 @@ styleElement.textContent = `
   font-family: 'Inter';
   font-style: normal;
   font-weight: 700;
+  font-display: swap;
   src: url('${fontUrl}Inter-Bold.woff2') format('woff2');
 }
 
+@font-face {
+  font-family: 'NotoSansKR';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('${fontUrl}NotoSansKR-Regular.woff2') format('woff2');
+  unicode-range: U+1100-11FF, U+3130-318F, U+AC00-D7A3;
+}
+
+@font-face {
+  font-family: 'NotoSansKR';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: url('${fontUrl}NotoSansKR-Medium.woff2') format('woff2');
+  unicode-range: U+1100-11FF, U+3130-318F, U+AC00-D7A3;
+}
+
+@font-face {
+  font-family: 'NotoSansKR';
+  font-style: normal;
+  font-weight: 700;
+  font-display: swap;
+  src: url('${fontUrl}NotoSansKR-Bold.woff2') format('woff2');
+  unicode-range: U+1100-11FF, U+3130-318F, U+AC00-D7A3;
+}
+`;
+container.appendChild(style2);
+
+const shadowRoot = container.attachShadow({ mode: "open" });
+const styleElement = document.createElement("style");
+styleElement.textContent = `
 :host {
   all: initial;
 }
 
 * {
-  font-family: 'Inter', system-ui;
+  font-family: 'NotoSansKR', 'Inter';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
